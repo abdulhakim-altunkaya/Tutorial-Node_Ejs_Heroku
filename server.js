ded07d41cb2735b6fb509f8117ec48d57a3f0132
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-03-04 18:13:19 +0300

    node-express-ejs-git-heroku exercise created

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, './public')));
+
+app.get("/", (req, res) => {
+    res.render("home");
+})
+app.get("/about", (req, res) => {
+    res.render("about");
+})
+app.get("/articles", (req, res) => {
+    res.render("about");
+})
+
+
+const server = app.listen(process.env.PORT || 4000);
+const portNumber = server.address().port;
+console.log(`port is open on ${portNumber}`);
